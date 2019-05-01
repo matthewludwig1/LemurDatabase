1ecc85c7b7c6fd89417af5df9bdc10999c30f405
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-05-01 11:48:01 -0400

    Update LemurDatabase.java

--- src/lemurdatabase/LemurDatabase.java
+++ src/lemurdatabase/LemurDatabase.java
@@ -1,8 +1,7 @@
 /*
     Matt Ludwig
-    04-30-2019
-    This program generates lemur objects, using the Mammal class, and the Lemur
-    class
+    05-01-2019
+    This program allows the user to create different types of lemurs, using
  */
 package lemurdatabase;
 
@@ -10,13 +9,38 @@ package lemurdatabase;
  *
  * @author malud0519
  */
+import javax.swing.*;
 public class LemurDatabase {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int numLem, typeLem;
+        numLem = Integer.parseInt(JOptionPane.showInputDialog("How many Lemurs do you want"
+                + " to add?"));
+        String lemurs[] = new String[numLem];   
+        for (int i = 0; i < numLem; i++) {
+            lemurs[i] = "";
+            typeLem = Integer.parseInt(JOptionPane.showInputDialog("1-Tree Lemur" + "\n"
+            + "2-Desert Lemur" + "\n" + "3-Jungle Lemur"));
+            
+            if (typeLem == 1) {
+                lemurs[i] += "Tree Lemur."; // could construct lemur here
+                //construct lemurs[i] : lemurs[i] = new TreeLemur();
+            }
+            
+            else if (typeLem == 2) {
+                lemurs[i] += "Desert Lemur.";
+            }
+            
+            else if (typeLem == 3) {
+                lemurs[i] += "Jungle Lemur.";
+            }
+            System.out.println("Creating ");
+        }
+        System.out.println("Displaying the Lemurs:" + "\n" + "-------------------------------"
+                + "------" + "\n \n");
         
     }
-    
 }
