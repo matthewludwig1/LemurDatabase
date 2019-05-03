53811428cd69037a971d68a7faee83583e9f68a2
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-05-03 11:23:28 -0400

    Update Lemur.java

--- src/lemurdatabase/Lemur.java
+++ src/lemurdatabase/Lemur.java
@@ -1,6 +1,6 @@
 /*
     Matt Ludwig
-    04-30-2019
+    05-03-2019
     This class holds the characteristics of Lemurs
  */
 package lemurdatabase;
@@ -13,9 +13,16 @@ public class Lemur extends Mammal{
     /* needs variables for the weight, gender etc, into an array, which
        will be passed into the subclasses. This class contains the common 
        attributes of Lemurs. 
-    
     */
-    // a default is not necessary
+    private String location, coat, classification;
     
-  
+    public String getLocation() {  // alternate, in case the toString does not work
+        location = "Madagascar";
+        return location;
+    }
+    public String getCoat() {
+        coat = "Fur";
+        return coat;
+    }
+     
 }
